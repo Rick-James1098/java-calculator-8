ecf1c3dc3c87ed684a7ab25fca66958f0e77e654
gksdnwls <gksdnwls1098@naver.com>
2025-10-20 20:52:22 +0900

    feat(ApplicationTest.java) : add an exception handling test case on README.md and corrected typo in README.md

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -17,12 +17,74 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void 양의_실수_사용() { // 정확한 실수 계산
+        assertSimpleTest(() -> {
+            run("0.1,0.2,0.3");
+            assertThat(output().contains("결과 : 0.6"));
+        });
+    }
+
+    @Test
+    void input_error() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("-1,2,3"))
                 .isInstanceOf(IllegalArgumentException.class)
         );
     }
+
+    @Test
+    void custom_separator_input_format() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//!n1,2,3"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void multiple_custom_separator() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//.\\n//;\\n1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void dot_defined_to_custom_separator() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//.\\n1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void start_or_end_with_comma() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(",1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,2,3,"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void double_separator() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void not_registered_separator() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1;2;3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
 
     @Override
     public void runMain() {
